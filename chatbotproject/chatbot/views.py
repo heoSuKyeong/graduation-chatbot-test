fb93f23d8531c95e4447dd66668d6f34bdfac5af
Lee Minji <minji3540@hanyang.ac.kr>
2024-11-30 19:53:27 +0900

    Change subcategory url and response format
    Signed-off-by: Lee Minji <minji3540@hanyang.ac.kr>

--- chatbotproject/chatbot/views.py
+++ chatbotproject/chatbot/views.py
@@ -8,18 +8,16 @@ from .models import MainCategory , SubCategory
 def index(request):
     return HttpResponse("안녕하세요. 기본 페이지입니다.")
 
-@api_view(['GET'])
-def getMainCaterogy(request):
-    main_categories = MainCategory.objects.all()
-    # return render(request, 'index.html', {'main_categories': main_categories})
-    serializer = mainCategorySerializer(main_categories, many=True)
-    return Response(serializer.data)
 
 @api_view(['GET'])
+def get_main_categories(request):
+    main_categories = MainCategory.objects.all()
+    serializer = mainCategorySerializer(main_categories, many=True)
+    return JsonResponse({'data': serializer.data})
+
+
+@api_view(['GET']) 
 def get_sub_categories(request, main_category_id):
     sub_categories = SubCategory.objects.filter(main_category_id=main_category_id)
     sub_categories_list = [{'id': sub_cat.id, 'name': sub_cat.name} for sub_cat in sub_categories]
-    return JsonResponse({'sub_categories': sub_categories_list})
-
-    # serializer = subCategorySerializer(sub_categories, many=True)
-    # return Response(serializer.data)
+    return JsonResponse({'data': sub_categories_list})
